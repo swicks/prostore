ec0e14a2fdf9e807eec5e9a8c8a38f85264b1fde
Steve Wicks <swicks@devicesoftware.com>
2025-01-01 16:41:47 +0000

    header & footer

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/shared/header";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen justify-center flex-col">
+      <Header />
+      <main className="flex-1 wrapper">{children}</main>
+    </div>
+  );
+}
