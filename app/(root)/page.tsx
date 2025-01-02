1db41fb2d2790e1f1421027b5eec8dd2514af0f4
Steve Wicks <swicks@devicesoftware.com>
2025-01-02 12:43:40 +0000

    loading & not-found pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
-const Homepage = () => {
+// const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+const Homepage = async () => {
+  // await delay(2000);
   return <>Prostore</>;
 };
-
 export default Homepage;
